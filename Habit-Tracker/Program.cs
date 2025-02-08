0fe828193ba797ce169c8b8fbad7a17a6adbc43f
rholexxdizon <dizonrholex02@gmail.com>
2025-02-08 23:51:05 +0800

    Initial commit

--- Habit-Tracker/Program.cs
+++ Habit-Tracker/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Data.Sqlite;
+
+namespace Habit_Tracker;
+
+class Program
+{
+    static string connectionString = @"Data Source=habit-tracker.db";
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Habit Tracker");
+        Console.WriteLine("Main Menu");
+        Console.WriteLine("1 - Create a habit");
+        Console.WriteLine("2 - Choose existing habit");
+        Console.WriteLine("3 - Exit");
+
+        string userInput = Console.ReadLine();
+
+        switch(userInput)
+        {
+            case "1":
+                CreateHabit();
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    private static void CreateHabit()
+    {
+        Console.WriteLine("Name of the Habit:");
+    }
+}
